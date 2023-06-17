9502807f273cef7e580f27b7f0420b095d8fdc53
Dhruvil Hitesh Prajapati <91490304+Dhruvil2511@users.noreply.github.com>
2023-06-17 16:50:32 +0530

    Create Number of Islands.cpp

---
+++ Graphs/Number of Islands.cpp
@@ -0,0 +1,30 @@
+// DFS Algorithm to solve this problem
+class Solution {
+public:
+    void dfs(int row,int col,vector<vector<char>>&grid){
+        if(row<0 || row>grid.size()-1 || col<0 || col>grid[row].size()-1 || grid[row][col]=='0'){
+            return;
+        }
+        grid[row][col]='0';
+        dfs(row+1,col,grid);
+        dfs(row-1,col,grid);
+        dfs(row,col+1,grid);
+        dfs(row,col-1,grid);
+    }
+    int numIslands(vector<vector<char>>& grid) {
+        int rows = grid.size();
+        int cols = grid[0].size();
+        int numOfIslands=0;
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                if(grid[i][j]=='1'){
+                    numOfIslands++;
+                    dfs(i,j,grid);
+                }
+            }
+        }   
+        return numOfIslands;
+    }
+};
+
+// Using connected components to solve this
