80b28a4df0786b2ed55551078d8aa171bb9d3ab4
Dhruvil Hitesh Prajapati <91490304+Dhruvil2511@users.noreply.github.com>
2023-07-15 10:50:49 +0530

    Update course_schedule.cpp
    All course schedule variation in this file

--- Graphs/course_schedule.cpp
+++ Graphs/course_schedule.cpp
@@ -33,3 +33,87 @@ public:
         return coursesDone==V;
     }
 };
+
+// course schedule 2
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        // creating adjList or creating graph
+        unordered_map<int,set<int>> adjList;
+        unordered_map<int,int> indegree;
+
+        for(auto prerequisite: prerequisites){
+            adjList[prerequisite[1]].insert(prerequisite[0]);
+            indegree[prerequisite[0]]++;
+        }
+
+        // bfs kahans algo
+        vector<int> ordering;
+
+        queue<int> q;
+        for(int i=0;i<numCourses;i++){
+            if(indegree[i]==0)q.push(i);
+        }
+
+        while(!q.empty()){
+            int front = q.front();
+            q.pop();
+            ordering.push_back(front);
+
+            for(auto node : adjList[front]){
+                indegree[node]--;
+                if(indegree[node]==0)q.push(node);
+            }
+        }
+
+        if(ordering.size()==numCourses)return ordering;
+        return {};
+
+    }
+};
+
+
+
+// course schedule 4
+
+
+class Solution {
+public:
+    vector<bool> checkIfPrerequisite(int numCourses, vector<vector<int>>& prerequisites, vector<vector<int>>& queries) {
+        // creating graph
+
+        unordered_map<int,set<int>> adjList;
+        unordered_map<int,int> indegree;
+        vector<vector<bool>> table(numCourses,vector<bool>(numCourses,false));
+
+        for(const auto prerequisite: prerequisites){
+            adjList[prerequisite[0]].insert(prerequisite[1]);
+            indegree[prerequisite[1]]++;
+        }
+
+        queue<int> q;
+        vector<bool> ans;
+        // kahns bfs
+
+        for(int i=0;i<numCourses;i++) if(indegree[i]==0) q.push(i);
+
+        while(!q.empty()){
+            int front = q.front();
+            q.pop();
+
+            for(auto node: adjList[front]){
+
+                table[front][node]=true;
+                for(int i=0;i<numCourses;i++)if(table[i][front]) table[i][node]=true;
+
+                indegree[node]--;
+                if(indegree[node]==0) q.push(node);
+            }
+        }
+
+        for(int i=0;i<queries.size();i++){
+            ans.push_back(table[queries[i][0]][queries[i][1]]);
+        }
+        return ans;
+    }
+};
